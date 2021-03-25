bf46f8c971ce53007b0892cd7764fca1b976fcb8
Rohit Jire <rohitjire55@gmail.com>
2021-03-25 16:27:35 +0530

    database connection done

---
+++ projbackend/app.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log("DB CONNNECTED");
+  })
+  .catch(console.log("DB CRASHED"));
+
+  
+app.use(bodyParser.json())
+app.use(cookieParser())
+app.use(cors())
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(8000, () => {
+  console.log(`app is running at ${port}`);
+});
