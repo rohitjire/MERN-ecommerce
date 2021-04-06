c96454205114cf91a3c493db09f08401d6d56398
Rohit Jire <rohitjire55@gmail.com>
2021-04-06 20:40:10 +0530

    frontend project setup done

---
+++ projfrontend/src/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "./core/Home"
+
+export default function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Home} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
---
+++ projfrontend/src/core/Home.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { API } from "../backend";
+import "../styles.css";
+import Base from "./Base";
+
+export default function Home() {
+  console.log("API IS", API);
+  return (
+    <Base>
+      <div className="row">
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+        <div className="col-4">
+          <button className="btn btn-success">TEST</button>
+        </div>
+      </div>
+    </Base>
+  );
+}
