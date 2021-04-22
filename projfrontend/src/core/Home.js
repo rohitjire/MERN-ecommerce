630f733c20b11b4a95ea35c51e875edfd5dfed75
Rohit Jire <rohitjire55@gmail.com>
2021-04-22 18:11:42 +0530

    home page loads all tshirts

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -1,21 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { getProducts } from "../admin/helper/adminapicall";
 import { API } from "../backend";
 import "../styles.css";
 import Base from "./Base";
 import Card from "./Card";
 
 export default function Home() {
+  const [Produtcs, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProducts = () => {
+    getProducts().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProducts()
+  }, [])
+
   return (
     <Base title="Home Page" description="Welcome to the Tshirt Store">
       <div className="row text-center">
-        <div className="col-4">
-          <Card></Card>
-        </div>
-        <div className="col-4">
-          <Card></Card>
-        </div>
-        <div className="col-4">
-          <Card></Card>
+        <h1 className="text-white">All of Tshirts</h1>
+        <div className="row mt-5">
+          {Produtcs.map((product,index)=>{
+            return(
+              <div key={index}className="col-3 mb-4">
+                <Card product = {product}>
+
+                </Card>
+              </div>
+            )
+          })}
         </div>
       </div>
     </Base>
