93a057824b531fed4a653296996de16596e80b4f
Rohit Jire <rohitjire55@gmail.com>
2021-04-22 11:53:41 +0530

    card created

--- projfrontend/src/core/Home.js
+++ projfrontend/src/core/Home.js
@@ -2,20 +2,20 @@ import React from "react";
 import { API } from "../backend";
 import "../styles.css";
 import Base from "./Base";
+import Card from "./Card";
 
 export default function Home() {
-  console.log("API IS", API);
   return (
     <Base title="Home Page" description="Welcome to the Tshirt Store">
-      <div className="row">
+      <div className="row text-center">
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card></Card>
         </div>
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card></Card>
         </div>
         <div className="col-4">
-          <button className="btn btn-success">TEST</button>
+          <Card></Card>
         </div>
       </div>
     </Base>
